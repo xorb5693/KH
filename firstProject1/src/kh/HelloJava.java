061c7be0e16973a688f847b03a835c38d8fc871c
xorb5693 <xorb5693@naver.com>
2020-01-29 10:06:36 +0900

    change test

--- firstProject1/src/kh/HelloJava.java
+++ firstProject1/src/kh/HelloJava.java
@@ -7,13 +7,13 @@ public class HelloJava {
 	public static void main(String[] args) {
 		
 		try {
-			BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+			BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); //키보드 입력
 			String str;
 				
 			for (int i = 0; i < 10; i++) {
 				str = bf.readLine();
 				System.out.println(str);
-//				System.out.println("test");
+				System.out.println("test");
 			}
 		} catch (IOException ie) {
 			System.err.println(1);
