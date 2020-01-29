390bd168243ec5d17c99bae7750a578f8bc53e37
xorb5693 <xorb5693@naver.com>
2020-01-29 16:29:14 +0900

    Commit

--- firstProject/src/kh/java/HelloJava.java
+++ firstProject/src/kh/java/HelloJava.java
@@ -3,7 +3,11 @@ package kh.java;
 public class HelloJava {
 
 	public static void main(String[] args) {
-		
+
+		if (true) {
+			System.out.println("test");
+		}
+
 		System.out.println("HelloJAVA!");
 		System.out.println("¾È³ç");
 	}
