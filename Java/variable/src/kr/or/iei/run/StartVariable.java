5e045ec5038b3503dd8e1402b55790b82f9f335e
xorb5693 <xorb5693@naver.com>
2020-02-06 11:41:10 +0900

    Variable Test Commit Change 4

--- Java/variable/src/kr/or/iei/run/StartVariable.java
+++ Java/variable/src/kr/or/iei/run/StartVariable.java
@@ -8,10 +8,10 @@ public class StartVariable {
 	public static void main(String[] args) {
 		
 		Example ex = new Example();
-//		ex.exam1();
-//		ex.exam2();
-//		ex.exam3();
-//		ex.exam4();
+		ex.exam1();
+		ex.exam2();
+		ex.exam3();
+		ex.exam4();
 		try {
 			ex.scan();
 		} catch (InputMismatchException e) {
