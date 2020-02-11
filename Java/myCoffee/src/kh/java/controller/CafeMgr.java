1f352084e13baf1f4f9d895cb9a6f7fe2e23f39d
xorb5693 <xorb5693@naver.com>
2020-02-11 23:09:58 +0900

    1st Team Project Coffee Manger Commit Change

--- Java/myCoffee/src/kh/java/controller/CafeMgr.java
+++ Java/myCoffee/src/kh/java/controller/CafeMgr.java
@@ -48,18 +48,18 @@ public class CafeMgr {
 		c1[cIndex] = new Coffee("아메리카노", 1000, 0, false, true);
 		c2[cIndex++] = new Coffee("아메리카노", 1000, 100, false, true);
 		c[cIndex] = new Coffee("아메리카노", 2500, 20, true, false);
-		c1[cIndex] = new Coffee("아메리카노", 1200, 0, true, false);
-		c2[cIndex++] = new Coffee("아메리카노", 1200, 100, true, false);
+		c1[cIndex] = new Coffee("아메리카노", 1250, 0, true, false);
+		c2[cIndex++] = new Coffee("아메리카노", 1250, 100, true, false);
 		c[cIndex] = new Coffee("아메리카노", 2500, 20, true, true);
-		c1[cIndex] = new Coffee("아메리카노", 1200, 0, true, true);
-		c2[cIndex++] = new Coffee("아메리카노", 1200, 100, true, true);
+		c1[cIndex] = new Coffee("아메리카노", 1250, 0, true, true);
+		c2[cIndex++] = new Coffee("아메리카노", 1250, 100, true, true);
 
 		c[cIndex] = new Coffee("카페라떼", 2500, 20, false, false);
-		c1[cIndex] = new Coffee("카페라떼", 1200, 0, false, false);
-		c2[cIndex++] = new Coffee("카페라떼", 1200, 100, false, false);
+		c1[cIndex] = new Coffee("카페라떼", 1250, 0, false, false);
+		c2[cIndex++] = new Coffee("카페라떼", 1250, 100, false, false);
 		c[cIndex] = new Coffee("카페라떼", 2500, 20, false, true);
-		c1[cIndex] = new Coffee("카페라떼", 1200, 0, false, true);
-		c2[cIndex++] = new Coffee("카페라떼", 1200, 100, false, true);
+		c1[cIndex] = new Coffee("카페라떼", 1250, 0, false, true);
+		c2[cIndex++] = new Coffee("카페라떼", 1250, 100, false, true);
 		c[cIndex] = new Coffee("카페라떼", 3000, 20, true, false);
 		c1[cIndex] = new Coffee("카페라떼", 1500, 0, true, false);
 		c2[cIndex++] = new Coffee("카페라떼", 1500, 100, true, false);
@@ -74,32 +74,32 @@ public class CafeMgr {
 		c1[cIndex] = new Coffee("카라멜마끼아또", 1500, 0, false, true);
 		c2[cIndex++] = new Coffee("카라멜마끼아또", 1500, 100, false, true);
 		c[cIndex] = new Coffee("카라멜마끼아또", 3500, 20, true, false);
-		c1[cIndex] = new Coffee("카라멜마끼아또", 1700, 0, true, false);
-		c2[cIndex++] = new Coffee("카라멜마끼아또", 1700, 100, true, false);
+		c1[cIndex] = new Coffee("카라멜마끼아또", 1750, 0, true, false);
+		c2[cIndex++] = new Coffee("카라멜마끼아또", 1750, 100, true, false);
 		c[cIndex] = new Coffee("카라멜마끼아또", 3500, 20, true, true);
-		c1[cIndex] = new Coffee("카라멜마끼아또", 1700, 0, true, true);
-		c2[cIndex++] = new Coffee("카라멜마끼아또", 1700, 100, true, true);
+		c1[cIndex] = new Coffee("카라멜마끼아또", 1750, 0, true, true);
+		c2[cIndex++] = new Coffee("카라멜마끼아또", 1750, 100, true, true);
 
 		j[jIndex] = new Juice("오렌지 주스", 3000, 15, false);
 		j1[jIndex] = new Juice("오렌지 주스", 1500, 0, false);
 		j2[jIndex++] = new Juice("오렌지 주스", 1500, 100, false);
 		j[jIndex] = new Juice("오렌지 주스", 3500, 15, true);
-		j1[jIndex] = new Juice("오렌지 주스", 1700, 0, true);
-		j2[jIndex++] = new Juice("오렌지 주스", 1700, 100, true);
+		j1[jIndex] = new Juice("오렌지 주스", 1750, 0, true);
+		j2[jIndex++] = new Juice("오렌지 주스", 1750, 100, true);
 
 		j[jIndex] = new Juice("딸기 주스", 3000, 15, false);
 		j1[jIndex] = new Juice("딸기 주스", 1500, 0, false);
 		j2[jIndex++] = new Juice("딸기 주스", 1500, 100, false);
 		j[jIndex] = new Juice("딸기 주스", 3500, 15, true);
-		j1[jIndex] = new Juice("딸기 주스", 1700, 0, true);
-		j2[jIndex++] = new Juice("딸기 주스", 1700, 100, true);
+		j1[jIndex] = new Juice("딸기 주스", 1750, 0, true);
+		j2[jIndex++] = new Juice("딸기 주스", 1750, 100, true);
 
 		j[jIndex] = new Juice("바나나 주스", 3000, 15, false);
 		j1[jIndex] = new Juice("바나나 주스", 1500, 0, false);
 		j2[jIndex++] = new Juice("바나나 주스", 1500, 100, false);
 		j[jIndex] = new Juice("바나나 주스", 3500, 15, true);
-		j1[jIndex] = new Juice("바나나 주스", 1700, 0, true);
-		j2[jIndex++] = new Juice("바나나 주스", 1700, 100, true);
+		j1[jIndex] = new Juice("바나나 주스", 1750, 0, true);
+		j2[jIndex++] = new Juice("바나나 주스", 1750, 100, true);
 
 		d[dIndex] = new Dessert("치즈 케이크", 3000, 15, false, "2020-02-11");
 		d1[dIndex] = new Dessert("치즈 케이크", 1500, 0, false, "2020-02-11");
@@ -157,7 +157,7 @@ public class CafeMgr {
 				case 0:
 					return;
 				default:
-					System.out.println("0~2 사이의 숫자를 입력해 주세요.");
+					System.out.println("0~3 사이의 숫자를 입력해 주세요.");
 				}
 			} catch (NumberFormatException ne) {
 				System.err.println("숫자를 입력해 주세요.");
@@ -207,30 +207,104 @@ public class CafeMgr {
 			}
 		}
 	}
-	
+
 	public void insertProduct() {
-		
+		while (true) {
+			System.out.println("===== 제품 등록 ======");
+			System.out.println("1. 커피 등록");
+			System.out.println("2. 과일 주스 등록");
+			System.out.println("3. 디저트 등록");
+			System.out.println("4. 기타 등록");
+			System.out.println("0. 이전으로");
+			System.out.print("선택 > ");
+
+			try {
+				int select = Integer.parseInt(br.readLine());
+
+				if (select == 0) {
+					System.out.println("이전 페이지로 돌아갑니다.");
+					delay();
+					return;
+				} else if (select > 0 && select < 5) {
+					System.out.print("상품명을 입력하세요. : ");
+					String name = br.readLine();
+
+					System.out.print("상품 가격을 입력하세요. : ");
+					int price = Integer.parseInt(br.readLine());
+
+					System.out.print("상품 수량을 입력하세요. : ");
+					int stock = Integer.parseInt(br.readLine());
+
+					switch (select) {
+					case 1:
+						System.out.print("크기[1. Small/2. Large] : ");
+						boolean size = (Integer.parseInt(br.readLine()) == 1) ? false : true;
+
+						System.out.print("온도[1. Hot/2. Cold] : ");
+						boolean temp = (Integer.parseInt(br.readLine()) == 1) ? false : true;
+
+						c[cIndex] = new Coffee(name, price, stock, size, temp);
+						c1[cIndex] = new Coffee(name, price / 2, 0, size, temp);
+						c2[cIndex++] = new Coffee(name, price / 2, 100, size, temp);
+						break;
+					case 2:
+						System.out.print("크기[1. Small/2. Large] : ");
+						size = (Integer.parseInt(br.readLine()) == 1) ? false : true;
+
+						j[jIndex] = new Juice(name, price, stock, size);
+						j1[jIndex] = new Juice(name, price / 2, 0, size);
+						j2[jIndex++] = new Juice(name, price / 2, 100, size);
+						break;
+					case 3:
+						System.out.print("크기[1. Small/2. Large] : ");
+						size = (Integer.parseInt(br.readLine()) == 1) ? false : true;
+
+						System.out.print("유통기한(yyyy-mm-dd) : ");
+						String expired = br.readLine();
+
+						d[dIndex] = new Dessert(name, price, stock, size, expired);
+						d1[dIndex] = new Dessert(name, price / 2, 0, size, expired);
+						d2[dIndex++] = new Dessert(name, price / 2, 100, size, expired);
+						break;
+					case 4:
+						etc[eIndex] = new Goods(name, price, stock);
+						etc1[eIndex] = new Goods(name, price / 2, 0);
+						etc2[eIndex++] = new Goods(name, price / 2, 100);
+						break;
+					}
+					System.out.println("제품 등록이 완료되었습니다.");
+				} else {
+					System.out.println("0~4 사이의 숫자를 입력하세요.");
+				}
+				delay();
+			} catch (NumberFormatException ne) {
+				System.err.println("숫자를 입력해 주세요.");
+			} catch (IOException ie) {
+				ie.printStackTrace();
+			}
+		}
 	}
-	
+
 	public void printCoffee() {
-		
+
 		System.out.println("===== 커피 =====");
 		System.out.println("No.\t이름\t\t\t가격\t재고");
-		
+
 		for (int i = 0; i < cIndex; i++) {
-			String name = (c[i].getTemp() ? "아이스" : "뜨거운") + " " + c[i].getName() + " " + (c[i].getSize() ? "Large" : "Small") + "\t";
+			String name = (c[i].getTemp() ? "아이스" : "뜨거운") + " " + c[i].getName() + " "
+					+ (c[i].getSize() ? "Large" : "Small") + "\t";
 			for (int k = 0; k < (23 - name.length()) / 8; k++) {
 				name += "\t";
 			}
 			System.out.println((i + 1) + "\t" + name + c[i].getPrice() + "\t" + c[i].getStock());
 		}
-		
+
 	}
-	
+
 	public void printJuice() {
 		System.out.println("===== 과일 주스 =====");
 		System.out.println("No.\t이름\t\t가격\t재고");
-		
+
 		for (int i = 0; i < jIndex; i++) {
 			String name = j[i].getName() + " " + (j[i].getSize() ? "Large" : "Small") + "\t";
 			for (int k = 0; k < (15 - name.length()) / 8; k++) {
@@ -239,24 +313,25 @@ public class CafeMgr {
 			System.out.println((i + 1) + "\t" + name + j[i].getPrice() + "\t" + j[i].getStock());
 		}
 	}
-	
+
 	public void printDesert() {
 		System.out.println("===== 디저트 =====");
 		System.out.println("No.\t이름\t\t가격\t재고\t유통기한");
-		
+
 		for (int i = 0; i < dIndex; i++) {
 			String name = d[i].getName() + " " + (d[i].getSize() ? "Large" : "Small") + "\t";
 			for (int k = 0; k < (15 - name.length()) / 8; k++) {
 				name += "\t";
 			}
-			System.out.println((i + 1) + "\t" + name + d[i].getPrice() + "\t" + d[i].getStock() + "\t" + d[i].getExpired());
+			System.out.println(
+					(i + 1) + "\t" + name + d[i].getPrice() + "\t" + d[i].getStock() + "\t" + d[i].getExpired());
 		}
 	}
-	
+
 	public void printEtc() {
 		System.out.println("===== 기타 =====");
 		System.out.println("No.\t이름\t가격\t재고");
-		
+
 		for (int i = 0; i < eIndex; i++) {
 			System.out.println((i + 1) + "\t" + etc[i].getName() + "\t" + etc[i].getPrice() + "\t" + etc[i].getStock());
 		}
