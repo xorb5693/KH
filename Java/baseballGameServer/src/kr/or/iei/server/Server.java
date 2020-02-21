da597877c6f88fcd53b24ef33c1532d7203b00ff
xorb5693 <xorb5693@naver.com>
2020-02-21 09:59:10 +0900

    18th Day Actual Problem 1 'baseballGameClient' Annotation Addition Commit

--- Java/baseballGameServer/src/kr/or/iei/server/Server.java
+++ Java/baseballGameServer/src/kr/or/iei/server/Server.java
@@ -5,108 +5,67 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Server {
 
-	int num[] = new int[3];
+	ArrayList<Integer> num = new ArrayList<Integer>();
 	Random rand = new Random();
 	int rank = 1;
 	int member = 0;
 
 	public void main() {
 
-		System.out.println("test");
-
 		int count = 0;
-		int i;
 
+		//3개의 0~9 사이의 숫자를 뽑기 위한 반복문
+		//반복문은 count가 3이 될 때 까지 반복한다.
 		while (count < 3) {
 			int number = rand.nextInt(10);
 
-			for (i = 0; i < count; i++) {
-				if (num[i] == number) {
-					break;
-				}
-			}
-
-			if (i == count) {
-				num[count++] = number;
+			//난수로 뽑은 숫자가 리스트에 존재하는지 확인한다.			
+			if (!num.contains(number)) {
+				//만약 난수로 뽑은 숫자가 리스트에 없다면(false) 리스트에 해당 숫자를 저장하고 count를 1증가 시킨다.
+				num.add(number);
+				count++;
 			}
 		}
 
 		try {
+			//ServerSocket 생성
 			ServerSocket serverSock = new ServerSocket(60001);
 
 			System.out.println("서버 소켓을 생성 했습니다.");
 			System.out.println("클라이언트의 접속을 기다립니다......");
-			System.out.println("서버숫자 -> " + num[0] + " " + num[1] + " " + num[2]);
+			System.out.println("서버숫자 -> " + num.get(0) + " " + num.get(1) + " " + num.get(2));
 
 			while (true) {
+				//클라이언트의 연결 요청이 올 때 까지 기다리고, 클라이언트가 연결이 되면 멤버를 1 증가시킨다.
 				Socket client = serverSock.accept();
 				member++;
 
 				System.out.println("클라이언트가 접속했습니다.");
 				System.out.println("현재 총 " + member + " 명 클라이언트");
 
-				// DataOutputStream dos = new DataOutputStream(client.getOutputStream());
-				// DataInputStream dis = new DataInputStream(client.getInputStream());
-				//
-				// while (true) {
-				// int strike = 0, ball = 0;
-				// int insertNum[] = new int[3];
-				// insertNum[0] = dis.readInt();
-				// insertNum[1] = dis.readInt();
-				// insertNum[2] = dis.readInt();
-				//
-				// System.out.println("클라이언트가 입력한 수 -> " + insertNum[0] + " " + insertNum[1] + "
-				// " + insertNum[2]);
-				//
-				// for (i = 0; i < 3; i ++) {
-				// if (num[i] == insertNum[i]) {
-				// strike++;
-				// }
-				// }
-				//
-				// for (i = 0; i < 3; i++) {
-				// for (int j = 0; j < 3; j++) {
-				// if (num[i] == insertNum[j] && i != j) {
-				// ball++;
-				// }
-				// }
-				// }
-				//
-				// System.out.println(strike + " 스트라이크 " + ball + " 볼");
-				//
-				// dos.writeInt(strike);
-				// dos.writeInt(ball);
-				//
-				// System.out.println();
-				//
-				// if (strike == 3) {
-				// System.out.println(strike + " 스트라이크 현재 클라이언트와 접속을 종료합니다.");
-				// member--;
-				// dos.writeInt(rank++);
-				// break;
-				// }
-				// }
-				//
-				// dis.close();
-				// dos.close();
-
+				//ServerThread 객체 생성.
+				//매개변수로 숫자 리스트와 현재 Server 객체, 그리고 클라이언트 Socket 객체를 넘긴다.
 				ServerThread serverThread = new ServerThread(num, this, client);
+				//스레드 시작
 				serverThread.start();
 			}
-//			client.close();
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
+	//클라이언트로 현재 랭크를 전송하고, member 숫자를 감소시키기 위한 메소드.
+	//매개변수로 클라이언트의 OutputStream을 받는다.
 	public void print(DataOutputStream dos) {
 		System.out.println("3 스트라이크 현재 클라이언트와 접속을 종료합니다.");
 		member--;
+		System.out.println("현재 접속된 클라이언트 : " + member + "명");
 		try {
 			dos.writeInt(rank++);
 		} catch (IOException e) {
--- Java/baseballGameServer/src/kr/or/iei/server/ServerThread.java
+++ Java/baseballGameServer/src/kr/or/iei/server/ServerThread.java
@@ -4,14 +4,16 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class ServerThread extends Thread {
 
-	int num[];
-	Server server;
-	Socket socket;
+	ArrayList<Integer> num = new ArrayList<Integer>();
+	Server server = null;
+	Socket socket = null;
 
-	public ServerThread(int num[], Server server, Socket socket) {
+	// 생성자. 매개변수로 숫자 배열과 Server 객체, 클라이언트의 Socket 객체를 받는다.
+	public ServerThread(ArrayList<Integer> num, Server server, Socket socket) {
 
 		this.num = num;
 		this.server = server;
@@ -22,55 +24,60 @@ public class ServerThread extends Thread {
 	public void run() {
 
 		try {
+			//클라이언트의 메시지를 받기 위한 dis와 클라이언트로 메시지를 전송하기 위한 dos
 			DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
 			DataInputStream dis = new DataInputStream(socket.getInputStream());
 
 			while (true) {
 				int strike = 0, ball = 0;
 				int insertNum[] = new int[3];
+				//readInt 메소드를 통해 3개의 숫자를 전송받는다.
 				insertNum[0] = dis.readInt();
 				insertNum[1] = dis.readInt();
 				insertNum[2] = dis.readInt();
 
 				System.out.println("클라이언트가 입력한 수 -> " + insertNum[0] + " " + insertNum[1] + " " + insertNum[2]);
 
+				//만약 같은 자리의 숫자가 동일하다면 strike를 1 증가시킨다.
 				for (int i = 0; i < 3; i++) {
-					if (num[i] == insertNum[i]) {
+					if (num.get(i) == insertNum[i]) {
 						strike++;
 					}
 				}
 
+				//클라이언트에서 입력받은 숫자를 contains 메소드를 통해 리스트에 있는지 확인하고 만약 있다면 ball을 1 증가시킨다.
 				for (int i = 0; i < 3; i++) {
-					for (int j = 0; j < 3; j++) {
-						if (num[i] == insertNum[j] && i != j) {
-							ball++;
-						}
+					if (num.contains(insertNum[i])) {
+						ball++;
 					}
 				}
+				
+				//실질적인 ball의 값은 strike를 뺀 값이다.
+				//이유는 ball을 연산하기 위한 contains 메소드는 같은 위치인 경우라도 true를 리턴하기에 strike인 경우에도 ball을 추가하기 때문이다. 
+				ball -= strike;
 
 				System.out.println(strike + " 스트라이크 " + ball + " 볼");
 
+				//클라이언트를 통해 strike와 ball을 전송한다.
 				dos.writeInt(strike);
 				dos.writeInt(ball);
 
 				System.out.println();
 
+				//만약 strike가 3이면 Server객체의 print 메소드를 호출한다.
 				if (strike == 3) {
 					server.print(dos);
 					break;
 				}
 			}
 
+			//클라이언트와의 스트림과 Socket을 닫는다.
 			dis.close();
 			dos.close();
-			serverClose(socket);
+			socket.close();
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
 		}
 	}
 
-	private void serverClose(Socket socket) throws IOException {
-		socket.close();
-	}
 }
