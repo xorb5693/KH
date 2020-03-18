6d781e870cc6653ece1bee33444742ffef73fe77
Park.T.G <xorb5693@naver.com>
2020-03-18 10:37:31 +0900

    JDBC Test Project Change Commit
    회원 정보 출력 프로그램 추가

--- Java/jdbc1/src/kh/java/DB/DBConnect.java
+++ Java/jdbc1/src/kh/java/DB/DBConnect.java
@@ -8,6 +8,63 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 public class DBConnect {
+	
+public ArrayList<Member> allMemberSearch() {
+		
+		ArrayList<Member> list = new ArrayList<Member>();
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rset = null;
+		
+//		드라이버 등록
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+//			Connection 객체 생성
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:xe", "jdbc", "1234");
+			
+//			Statemnt 객체 생성
+			stmt = conn.createStatement();
+			
+//			쿼리문 요청 후 결과 받기
+			String query = "select * from member order by member_id";
+			rset = stmt.executeQuery(query);
+		
+//			결과처리
+			
+			while (rset.next()) {
+				Member mem = new Member(
+						rset.getString("member_id"),
+						rset.getString("member_pw"),
+						rset.getString("member_name"),
+						((rset.getString("gender").charAt(0) == 'M') ? "남자" : "여자"),
+						rset.getInt("age"),
+						rset.getString("phone"),
+						rset.getString("hobby"),
+						rset.getDate("enroll_date")
+				);
+				
+				list.add(mem);
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+//			자원 반환
+			try {
+				rset.close();
+				stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		return list;
+	}
 
 	public Member idSearch(String memberId) {
 		Connection conn = null;
@@ -134,4 +191,143 @@ public class DBConnect {
 		
 		return list;
 	}
+
+	public int insertMember(Member member) {
+		
+		Connection conn = null;
+		Statement stmt = null;
+		int result = 0;
+		
+		String query = "insert into member values("
+				+ "'" + member.getMemberId() + "', "
+				+ "'" + member.getMemberPw()+ "', "
+				+ "'" + member.getMemberName() + "', "
+				+ "'" + member.getGender() + "', "
+				+ member.getAge() + ", "
+				+ "'" + member.getPhone() + "', "
+				+ "'" + member.getHobby() + "', "
+				+ "SYSDATE)";
+		
+//		System.out.println(query);
+		
+//		드라이버 등록
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+//			Connection 객체 생성
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:xe", "jdbc", "1234");
+			
+//			Statemnt 객체 생성
+			stmt = conn.createStatement();
+//			쿼리문 요청 후 결과 받기
+			result = stmt.executeUpdate(query);
+//			결과처리
+			if (result > 0) {
+				conn.commit();
+			} else {
+				conn.rollback();
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+//			자원 반환
+			try {
+				stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+	}
+
+	public int updateMember(Member member) {
+
+		// 비밀번호, 전화번호, 취미만 수정
+		Connection conn = null;
+		Statement stmt = null;
+		int result = 0;
+		
+		String query = "update member set "
+				+ "member_pw = '" + member.getMemberPw() + "', "
+				+ "phone = '" + member.getPhone() + "', "
+				+ "hobby = '" + member.getHobby() + "' "
+				+ "where member_id = '" + member.getMemberId() + "'";
+		
+//		System.out.println(query);
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:xe", "jdbc", "1234");
+			
+			stmt = conn.createStatement();
+			
+			result = stmt.executeUpdate(query);
+			
+			if (result > 0) {
+				conn.commit();
+			} else {
+				conn.rollback();
+			}
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+	}
+
+	public int deleteMember(String memberId) {
+		
+		Connection conn = null;
+		Statement stmt = null;
+		int result = 0;
+		
+		String query = "delete from member where member_id = '" + memberId + "'";
+		
+		try {
+			
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:xe", "jdbc", "1234");
+			
+			stmt = conn.createStatement();
+			
+			result = stmt.executeUpdate(query);
+			
+			if (result > 0) {
+				conn.commit();
+			} else {
+				conn.rollback();
+			}
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+	}
+	
 }
--- Java/jdbc1/src/kh/java/mgr/MemberMgr.java
+++ Java/jdbc1/src/kh/java/mgr/MemberMgr.java
@@ -7,56 +7,73 @@ import kh.java.DB.DBConnect;
 import kh.java.DB.Member;
 
 public class MemberMgr {
-	
+
 	Scanner sc = new Scanner(System.in);
 
 	public void main() {
-		
+
 		while (true) {
-			System.out.println("===== 회원관리 프로그램 =====");
-			System.out.println("1. 회원 전체 정보 조회");	//select
-			System.out.println("2. 회원 아이디로 조회");	//select
-			System.out.println("3. 회원 이름으로 조회");	//select
-			System.out.println("4. 회원 가입");			//insert
-			System.out.println("5. 회원 정보 변경");		//update
-			System.out.println("6. 회원 삭제");			//delete
+			System.out.println("\n===== 회원관리 프로그램 =====");
+			System.out.println("1. 회원 전체 정보 조회"); // select
+			System.out.println("2. 회원 아이디로 조회"); // select
+			System.out.println("3. 회원 이름으로 조회"); // select
+			System.out.println("4. 회원 가입"); // insert
+			System.out.println("5. 회원 정보 변경"); // update
+			System.out.println("6. 회원 삭제"); // delete
 			System.out.println("0. 프로그램 종료");
 			System.out.print("선택 > ");
 			int sel = sc.nextInt();
-			
+
 			switch (sel) {
-			case 1 :
+			case 1:
+				allMemberSeach();
 				break;
-			case 2 :
+			case 2:
 				idSearch();
 				break;
-			case 3 :
+			case 3:
 				nameSearch();
 				break;
-			case 4 :
+			case 4:
+				insertMember();
 				break;
-			case 5 :
+			case 5:
+				updateMember();
 				break;
-			case 6 :
+			case 6:
+				deleteMember();
 				break;
-			case 0 :
+			case 0:
 				return;
 			}
-			
+
 			delay();
 		}
 	}
-	
+
+	private void allMemberSeach() {
+
+		System.out.println("\n===== 전체 조회 =====");
+		System.out.println("아이디\t패스워드\t이름\t성별\t나이\t전화번호\t\t취미\t가입일");
+
+		DBConnect db = new DBConnect();
+		ArrayList<Member> members = db.allMemberSearch();
+
+		for (Member m : members) {
+			System.out.println(m);
+		}
+	}
+
 	public void idSearch() {
-		
-		System.out.println("===== 아이디로 조회 =====");
+
+		System.out.println("\n===== 아이디로 조회 =====");
 		System.out.print("조회할 아이디 입력 : ");
 		String memberId = sc.next();
-		
-		DBConnect db= new DBConnect();
-//		HashMap<String, Object> member = db.idSearch(memberId);
+
+		DBConnect db = new DBConnect();
+		// HashMap<String, Object> member = db.idSearch(memberId);
 		Member member = db.idSearch(memberId);
-		
+
 		if (member == null) {
 			System.out.println("아이디를 조회할 수 없습니다.");
 		} else {
@@ -70,37 +87,138 @@ public class MemberMgr {
 			System.out.println("가입일 : " + member.getEnrollDate());
 		}
 	}
-	
+
 	public void nameSearch() {
-		System.out.println("===== 이름으로 조회 =====");
+		System.out.println("\n===== 이름으로 조회 =====");
 		System.out.print("조회할 이름 입력 : ");
 		String memberName = sc.next();
-		
+
 		DBConnect db = new DBConnect();
 		ArrayList<Member> list = db.nameSearch(memberName);
-		
+
 		if (list.isEmpty()) {
 			System.out.println("이름을 조회할 수 없습니다.");
 		} else {
 			for (Member member : list) {
-//				System.out.println("===== 회원 정보 =====");
-//				System.out.println("아이디 : " + member.getMemberId());
-//				System.out.println("비밀번호 : " + member.getMemberPw());
-//				System.out.println("이름 : " + member.getMemberName());
-//				System.out.println("성별 : " + member.getGender());
-//				System.out.println("나이 : " + member.getAge());
-//				System.out.println("전화번호 : " + member.getPhone());
-//				System.out.println("취미 : " + member.getHobby());
-//				System.out.println("가입일 : " + member.getEnrollDate());
-//				System.out.println();
-				
+				// System.out.println("===== 회원 정보 =====");
+				// System.out.println("아이디 : " + member.getMemberId());
+				// System.out.println("비밀번호 : " + member.getMemberPw());
+				// System.out.println("이름 : " + member.getMemberName());
+				// System.out.println("성별 : " + member.getGender());
+				// System.out.println("나이 : " + member.getAge());
+				// System.out.println("전화번호 : " + member.getPhone());
+				// System.out.println("취미 : " + member.getHobby());
+				// System.out.println("가입일 : " + member.getEnrollDate());
+				// System.out.println();
+
 				System.out.println(member);
+			}
+		}
+	}
+
+	private void insertMember() {
+		System.out.println("\n===== 회원 가입 =====");
+		System.out.print("아이디 입력 : ");
+		String memberId = sc.next();
+
+		System.out.print("비밀번호 입력 : ");
+		String memberPw = sc.next();
+
+		System.out.print("이름 입력 : ");
+		String memberName = sc.next();
+
+		System.out.print("성별 입력(M/F) : ");
+		String gender = sc.next();
+
+		System.out.print("나이 입력 : ");
+		int age = sc.nextInt();
+
+		System.out.print("전화번호 입력(010-0000-0000) : ");
+		String phone = sc.next();
+
+		System.out.print("취미 입력 : ");
+		String hobby = sc.next();
+
+		Member member = new Member(memberId, memberPw, memberName, gender, age, phone, hobby, null);
+
+		DBConnect db = new DBConnect();
+		int result = db.insertMember(member);
+
+		if (result > 0) {
+			System.out.println("회원 가입 완료.");
+		} else {
+			System.out.println("회원 가입 실패. 관리자에게 문의하세요.");
+		}
+	}
+
+	private void updateMember() {
+
+		// 비밀번호, 전화번호, 취미만 수정
+		System.out.println("\n===== 회원 정보 수정 =====");
+		System.out.print("수정할 아이디 입력 : ");
+		String memberId = sc.next();
+
+		DBConnect db = new DBConnect();
+		Member mem = db.idSearch(memberId);
+
+		if (mem == null) {
+
+			System.out.println("존재하지 않는 아이디입니다.");
+		} else {
+
+			System.out.print("비밀번호 입력 : ");
+			String memberPw = sc.next();
+
+			System.out.print("전화번호 입력(010-0000-0000) : ");
+			String phone = sc.next();
+
+			System.out.print("취미 입력 : ");
+			String hobby = sc.next();
+
+			Member member = new Member(memberId, memberPw, null, null, 0, phone, hobby, null);
+			int result = db.updateMember(member);
+			
+			if (result > 0) {
+				System.out.println("회원 정보가 수정되었습니다.");
+			} else {
+				System.out.println("회원 정보 수정에 실패했습니다.");
+			}
+		}
+	}
+
+	private void deleteMember() {
+		
+		System.out.println("\n==== 회원 정보 삭제 ====");
+		System.out.print("삭제할 아이디를 입력하세요 : ");
+		String memberId = sc.next();
+		
+		DBConnect db = new DBConnect();
+		Member mem = db.idSearch(memberId);
+		
+		if (mem == null) {
+
+			System.out.println("존재하지 않는 아이디입니다.");
+		} else {
+			
+			System.out.print("정말로 삭제하시겠씁니까?[y/n] : ");
+			char ch = sc.next().charAt(0);
+			
+			if (ch == 'y') {
+				
+				int result = db.deleteMember(memberId);
+				if (result > 0) {
+					System.out.println("회원 정보를 삭제했습니다.");
+				} else {
+					System.out.println("회원 정보 삭제에 실패하였습니다.");
+				}
+			} else {
+				System.out.println("회원 정보 삭제를 취소하였습니다.");
 			}
 		}
 	}
 	
 	public void delay() {
-		
+
 		try {
 			Thread.sleep(500);
 		} catch (InterruptedException e) {
